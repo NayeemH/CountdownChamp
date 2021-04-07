de3810e64b25220b61e36263ff9c467fc6d29f13
NayeemH <nayeemh67@gmail.com>
2021-04-08 01:09:53 +0600

    Updating state by submit button

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,23 @@ import './App.css'
 
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            deadline: 'December 25, 2021'
+        }
+
+    }
+
+    changeDeadline(){
+        this.setState({deadline: 'November 25, 2021'})
+    }
+
+
     render(){
         return(
             <div className = "App">
-                <div className = "App-title">Countdown to December 25, 2021</div>
+                <div className = "App-title">Countdown to {this.state.deadline}</div>
                 <div>
                     <div className="Clock-days">14 Days </div>
                     <div className="Clock-hours">15 Hours </div>
@@ -15,7 +28,7 @@ class App extends Component {
                 </div>
                 <div>
                     <input placeholder ="New date"/>
-                    <button>Submit</button>
+                    <button onClick={() => this.changeDeadline()}>Submit</button>
                 </div>
                 
             </div>
